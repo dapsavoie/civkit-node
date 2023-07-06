95dadf535f11d584e9d280f80ea74e3258618a83
daven <daven.savoie@kureapp.com>
2023-07-06 13:50:13 +0100

    address 2 rust versions

--- ci/ubuntu/setup_env.sh
+++ ci/ubuntu/setup_env.sh
@@ -9,4 +9,13 @@ sudo apt-get install -y protobuf-compiler
 # Install rustup, the Rust toolchain installer
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+# Set the PATH to include .cargo/bin
+source $HOME/.cargo/env
+
+# Install the minimum supported Rust version (MSRV)
+rustup install 1.41.1
+
+# Install a newer version of Rust for development/testing
+rustup install 1.68.0
+
 echo "Environment setup completed."
