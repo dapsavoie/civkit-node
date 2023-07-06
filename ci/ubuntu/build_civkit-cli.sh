860a647be34ea86f07ef8a1359ade16669dbf4cd
daven <daven.savoie@kureapp.com>
2023-07-06 12:22:59 +0100

    check binary without target folder

--- ci/ubuntu/build_civkit-cli.sh
+++ ci/ubuntu/build_civkit-cli.sh
@@ -2,11 +2,10 @@
 
 set -o errexit
 
-cd ../../target/debug
-
-if cargo build --bin civkit-cli --target=x86_64-unknown-linux-gnu; then
-  echo "civkit-cli build: SUCCESS"
+# Check if the civkit-cli binary exists
+if [ -f "civkit-cli" ]; then
+    echo "civkit-cli build: SUCCESS"
 else
-  echo "civkit-cli build: FAILURE"
-  exit 1
+    echo "civkit-cli build: FAILURE"
+    exit 1
 fi
